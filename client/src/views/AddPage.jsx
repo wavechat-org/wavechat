3fa8120d6f71b6a9f82c4fab92b7f70e4b6ac9f7
wijaya2020 <wijaya181028@gmail.com>
2024-11-06 21:10:24 +0700

    feat: client homepage, card

--- client/src/views/AddPage.jsx
+++ client/src/views/AddPage.jsx
@@ -1,7 +1,58 @@
-export default function AddPage() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export default function AddPage({ base_url }) {
+  const [roomName, setRoomName] = useState("");
+  const [creatorId, setCreatorId] = useState(1);
+  const [participantId, setParticipantId] = useState(2);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const body = { roomName, creatorId, participantId };
+      const { data } = await axios.post(
+        `${base_url}/room`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.access_token}`,
+          },
+        }
+      );
+      navigate("/");
+      // Toastify
+    } catch (error) {
+      console.log(error);
+      // Toastify
+    }
+  }
+
   return (
-    <div>
-      <h1>Add Room Page</h1>
-    </div>
+    <section className="w-50 mx-auto p-3 border-1 rounder bg-warning-subtle">
+      <h1 className="text-center pt-2">Add Room</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="room-name" className="form-label">
+            Room Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="room-name"
+            onChange={(e) => setRoomName(e.target.value)}
+            value={roomName}
+          />
+        </div>
+
+        <div className="d-flex gap-3 pt-5">
+          <button type="submit" className="btn btn-warning w-100">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
   );
 }
--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,7 +1,48 @@
-export default function HomePage() {
+import Card from "../components/Card";
+import axios from "axios";
+import loadingAnimation from "../components/assets/Double Ring@1x-1.0s-200px-200px.svg";
+import { useEffect, useState } from "react";
+
+export default function HomePage({ base_url }) {
+  const [loading, setLoading] = useState(false);
+  const [rooms, setRooms] = useState([]);
+
+  async function fetchRooms() {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`${base_url}/room`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.access_token}`,
+        },
+      });
+      console.log(data, "<<<<<<<<<<<<<<<< data");
+
+      setRooms(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section>
+      <div className="d-flex flex-wrap gap-3 jutify-content-center py-5">
+        {rooms.map((room) => {
+          return (
+            <Card
+              key={room.id}
+              room={room}
+              base_url={base_url}
+              fetchRooms={fetchRooms}
+            />
+          );
+        })}
+      </div>
+    </section>
   );
 }
